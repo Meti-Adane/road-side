97bce49bc59d1db854bc30042a51046908380e93
MastewalB <se.mastewal.belete@gmail.com>
2021-08-26 11:01:02 +0300

    Register Form page and dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:road_side/presentation/core/main_screen.dart';
 import 'package:road_side/injection.dart';
 import 'package:injectable/injectable.dart';
+import 'package:road_side/presentation/sign_in/sign_in_page.dart';
 
 void main() {
   configureInjection(Environment.prod);
@@ -9,6 +10,6 @@ void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: 'Road Side',
-    home: MainScreen(),
+    home: SignInPage(),
   ));
 }
