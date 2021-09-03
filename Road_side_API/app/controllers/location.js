7954d73496c2b6700820eaaf8f05b4c755466a71
Meti-Adane <metiade7@gmail.com>
2021-09-03 23:11:21 +0300

    edit get nearby garages's response

--- Road_side_API/app/controllers/location.js
+++ Road_side_API/app/controllers/location.js
@@ -24,26 +24,18 @@ export const addGarageToLocation = (req, res) => {
 
 export const getNearByGarages = (req, res) => { //send partial data
     const locationName = req.body.location_name
-
     if (!locationName) res.status(400).send("BAD REQUEST all inputs are required").end()
-
     const query = { "name": area }
-    Location.find(query)
-    .then(data => {
-      res.status(200).send(data).end();
+    Location.find(query, 'available_garages', (err, data) => {
+        if (err) res.status(500).send(err || "SERVER INTERNAL ERROR").end()
+        if (!data) res.status(404).send("Could not find Available garages").end()
+        res.status(200).send({available_garages:data}).end()
     })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || " INTERNAL ERROR Some error occurred while retrieving."
-      }).end();
-    });
 }
 
 export const getAllLocations = (req, res) => {
     Location.find({}, 'names', function (err, garages) {
-        if (err) res.status(500).send('INTERNAL SERVER ERROR || Soemthign went wrong whilr retriving ');
-        res.status(201).send("OK").end()
-        
-      });
+        if (err) res.status(500).send('INTERNAL SERVER ERROR || Soemthign went wrong whilr retriving ').end();
+        res.status(201).send("OK").end()        
+    });
 }
