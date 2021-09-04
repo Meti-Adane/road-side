de6cf614f26106e7a8dcce6786044b0c7db19bd8
Meti-Adane <metiade7@gmail.com>
2021-09-04 18:50:50 +0300

    register loctaion routes

--- Road_side_API/app.js
+++ Road_side_API/app.js
@@ -3,6 +3,7 @@ import router from './app/routes/garage.js';
 import routes  from './app/routes/index.js';
 const userRouter = routes.userRouter;
 const garageRouter = routes.garageRouter
+const locationRouter = routes.locationRouter
 // import dotenv from 'dotenv';
 
 // dotenv.config();
@@ -13,7 +14,9 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use('/api/users', userRouter);
-app.use('/api/garages', garageRouter)
+app.use('/api/garages', garageRouter);
+app.use('/api/locations', locationRouter)
+
 
 
 export default app;
