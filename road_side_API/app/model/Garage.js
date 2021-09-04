2b9e68e0d0a671bde97da8baff504758dca6b213
Meti-Adane <metiade7@gmail.com>
2021-09-05 01:33:22 +0300

    modify garage model for token storing

--- road_side_API/app/model/Garage.js
+++ road_side_API/app/model/Garage.js
@@ -1,88 +1,83 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 // schema for contact of garage
-const contact_schema = mongoose.Schema(
-    {
-        email:{
-            type:String,
-            required:true,
-            lowercase:true,
-            unique: true,
-        },
-        phone_number:{
-            type: Array,
-            required: true,
-        },
-    }
-)
-
+const contact_schema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
+  },
+  phone_number: {
+    type: Array,
+    required: true,
+  },
+});
 
 const garage_schema = mongoose.Schema(
-    {
-        name: {
-            type:String,
-        },
-        username:{
-            type:String,
-            lowercase:true,
-            unique:true,
-
-        },
-        password:{
-            type: String,
-            required:true,
-            min:6,
-            
-        },
-        description : {
-            type:String,
-
-        },
-        location:{
-            type:String,
-        },
-        contact:{
-            type : [contact_schema],
-            default:undefined
-        },
-        opening_hour:{
-            type:String,
-            required:true,
-        },
-        closing_hour:{
-            type:String,
-            required:true,
-        },
-        services:[
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                required:true
-            }
-        ],
-            
-        ongoing_services : [ 
-            { // only order ids are stored for ongoing services
-                type : mongoose.Schema.Types.ObjectId,
-                ref: 'Order',
-            }
-        ]
-
+  {
+    name: {
+      type: String,
     },
-    {
-        toObject: {
-          transform: function (doc, ret) {
-            ret.id = ret._id
-            delete ret._id;
-          }
-        },
-        toJSON: {
-          transform: function (doc, ret) {
-            ret.id = ret._id
-            delete ret._id;
-          }
-        }
-    }
-)
+    user_name: {
+      type: String,
+      lowercase: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    description: {
+      type: String,
+    },
+    location: [String],
+    contact: {
+      type: [contact_schema],
+      default: undefined,
+    },
+    opening_hour: {
+      type: String,
+      required: true,
+    },
+    closing_hour: {
+      type: String,
+      required: true,
+    },
+    services: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+      },
+    ],
+
+    ongoing_services: [
+      {
+        // only order ids are stored for ongoing services
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Order",
+      },
+    ],
+    token: {
+      type: String,
+    },
+  },
+  {
+    toObject: {
+      transform: function (doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+    toJSON: {
+      transform: function (doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
 
 const Garage = mongoose.model("garage", garage_schema);
 
