c5a92c87203b9ea28d83af99632b1bb7d9b043fe
Meti-Adane <metiade7@gmail.com>
2021-09-04 18:46:34 +0300

    set up garage route

--- Road_side_API/app.js
+++ Road_side_API/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import router from './app/routes/garage.js';
 import routes  from './app/routes/index.js';
 const userRouter = routes.userRouter;
+const garageRouter = routes.garageRouter
 // import dotenv from 'dotenv';
 
 // dotenv.config();
@@ -11,6 +13,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use('/api/users', userRouter);
+app.use('/api/garages', garageRouter)
 
 
 export default app;
