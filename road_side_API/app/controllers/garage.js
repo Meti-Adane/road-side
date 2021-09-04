8974b5ebb3feb05f9765501ff12a7240a3425dbf
Meti-Adane <metiade7@gmail.com>
2021-09-05 01:32:05 +0300

    Register new garage

--- road_side_API/app/controllers/garage.js
+++ road_side_API/app/controllers/garage.js
@@ -1,33 +1,78 @@
-import Garage from '../model/Garage.js';
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import Garage from "../model/Garage.js";
 
-export const getAllGarages = (req, res) => {
+export const getAllGarages = (req, res) => {};
 
-}
-
-export const getGarageById = (req, res) => {
-
-}
+export const getGarageById = (req, res) => {};
 
 export const addNewGarage = (req, res) => {
-    const newGarage  = {
-        name: "Brown sugar",
-        username:"brown123",
-        password:"brown12345",
-        description : "The best auto repair shop",
-        location:{
-            type:String,
-        },
-        contact:[{email:"brown@gmail.com", phone_number:"0922654231"}, {email:"brown2@gmail.com", phone_number:"0933654231"}],
-        opening_hour:"6:00 AM",
-        closing_hour:"10:00PM",
-        services:[ ],
-    }
-}
+  let garage = req.body;
 
-export const deleteGarage = (req, res) => {
+  if (!(garage && Object.keys(garage).length >= 9)) {
+    return res
+      .status(400)
+      .send({ message: "BAD REQUEST missing inputs" })
+      .end();
+  }
+  const query = {
+    $or: [{ name: garage.name }, { user_name: garage.user_name }],
+  };
 
-}
+  Garage.find(query)
+    .then((data) => {
+      if (data.length) {
+        res
+          .status(409)
+          .send({ message: "CONFLICT Garage already in registered" })
+          .end();
+      } else {
+        //Encrypt password
+        bcrypt
+          .hash(garage.password, 10)
+          .then((data) => {
+            garage.password = data;
+            const newGarage = new Garage(garage);
+            newGarage
+              .save()
+              .then((data) => {
+                if (data) {
+                  const token = jwt.sign(
+                    { id: garage._id, user_name: garage.user_name },
+                    process.env.TOKEN_KEY,
+                    {
+                      expiresIn: "2h",
+                    }
+                  );
+                  console.log(token, "TOKEN");
+                  // save user token
+                  garage = data;
+                  garage.token = token;
+                  res.status(201).send({ message: "CREATED", garage }).end();
+                }
+              })
+              .catch((err) => {
+                res
+                  .status(500)
+                  .send({ message: "Error on saving garage" + err })
+                  .end();
+              });
+          })
 
-export const findNearByGarage = (req, res) => {
-    
-}
+          .catch((err) =>
+            res
+              .status(500)
+              .send(err || "HASHING")
+              .end()
+          );
+      }
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .send({ message: "Error on checking if garage exists" })
+        .end();
+    });
+};
+
+export const deleteGarage = (req, res) => {};
