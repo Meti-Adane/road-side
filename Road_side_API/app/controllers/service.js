d88511ec981035474368b5e5582997da17d1399c
Meti-Adane <metiade7@gmail.com>
2021-09-04 18:54:47 +0300

    add add services controller

---
+++ Road_side_API/app/controllers/service.js
@@ -0,0 +1,19 @@
+import Service from '../model/Service.js';
+export const addServices = (req, res) => {
+    const arrServices = [{name:"quick fix"}, {name: "towing"}, {name:"wash"}]
+    if (!arrServices) {
+        res.status(400).send({message: "BAD REQUEST BODY CANT BE EMPTY"}).end()
+    }
+    Service.insertMany(arrServices)
+    .then(data => {
+        if (!data) {
+            console.log("No data was sent back ")
+            res.status(204).send({message: "NO CONTENT", services: data}).end()
+        }
+        res.status(200).send({message: "Services Added", services: data}).end()
+    })
+    .catch (error => {
+        console.log(error)
+        res.status(500).send({message: "INTERNAL SERVER ERROR"}).end()
+    }) 
+}
