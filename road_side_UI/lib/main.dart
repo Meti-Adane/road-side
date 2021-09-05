60678102e685a834fcc42bac88fcdf4edaa8d511
Natnael-Alemayehu <61546827+Natnael-Alemayehu@users.noreply.github.com>
2021-09-05 20:33:39 +0300

    Main.dart had some minor modifications

--- road_side_UI/lib/main.dart
+++ road_side_UI/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:road_side/injection.dart';
 import 'package:injectable/injectable.dart';
 import 'package:road_side/presentation/main_screen/main_screen.dart';
+import 'package:road_side/presentation/setting/setting_screen.dart';
 import 'package:road_side/presentation/sign_in/sign_in_page.dart';
 import 'package:road_side/presentation/profile/profile_screen.dart';
 
@@ -13,8 +14,9 @@ Future<void> main() async {
       debugShowCheckedModeBanner: false,
       title: 'Road Side',
       // home: SignInPage(),
-      home: SettingsUI(),
+      // home: SettingsUI(),
       // home: MainScreen(),
+      home: SettingsPage(),
     ),
   );
 }
