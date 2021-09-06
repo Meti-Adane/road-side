9ffeab785984b7342e8047b41b2f1ec0679ce52e
Meti-Adane <metiade7@gmail.com>
2021-09-06 10:58:23 +0300

    modification on user model

--- road_side_API/app/model/User.js
+++ road_side_API/app/model/User.js
@@ -1,87 +1,84 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const rating_Schema = {
-    garage_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Garage'
-    },
-    rating: {
-        type: Number
-    }
-}
+  garage_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Garage",
+  },
+  rating: {
+    type: Number,
+  },
+};
 const userSchema = mongoose.Schema(
-    {
-        first_name:{
-            type : String,
-            required:true,            
-        },
-        last_name:{
-            type : String,
-            required:true,            
-        },
-        username:{
-            type:String,
-            lowercase:true,
-            unique:true,
-        },
-        password:{
-            type: String,
-            required:true,
-            min:6,            
-        },
-        role:{
-            type:String,
-            default:"User"
-        },
-        profile_picture:{
-            type: String            
-        },
-        phoneNumber:{
-            type : String,
-            required : true,
-            unique : true,
-            min:10,
-        },
-        email:{
-            type :String,
-            lowercase: true,
-            unique : true,
-        }, 
-        order_history :{
-            type:Array,
-        },
-        
-        ongoing_services :{ // can be retrived from order_history
-            type: Array,
+  {
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    user_name: {
+      type: String,
+      lowercase: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    role: {
+      type: String,
+      default: "User",
+      lowercase: true,
+    },
+    profile_picture: {
+      type: String,
+    },
+    phone_number: {
+      type: String,
+      required: true,
+      unique: true,
+      min: 10,
+    },
+    email: {
+      type: String,
+      lowercase: true,
+      unique: true,
+    },
+    order_history: {
+      type: Array,
+    },
 
-        },
-        favorites :{
-            type : [mongoose.Schema.Types.ObjectId],
-            ref: 'Garage',
-
-        },
-        rated_garages : {
-            type:[rating_Schema],
-        }
-
-    }, 
-    {
-        toObject: {
-          transform: function (doc, ret) {
-            ret.id = ret._id
-            delete ret._id;
-          }
-        },
-        toJSON: {
-          transform: function (doc, ret) {
-            ret.id = ret._id
-            delete ret._id;
-          }
-        }
-    }
+    ongoing_services: {
+      // can be retrived from order_history
+      type: Array,
+    },
+    favorites: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "Garage",
+    },
+    rated_garages: {
+      type: [rating_Schema],
+    },
+  },
+  {
+    toObject: {
+      transform: function (doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+    toJSON: {
+      transform: function (doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
 );
-
-
 
 const User = mongoose.model("user", userSchema);
 export default User;
