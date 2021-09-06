c18b3df821f83ea1a50d4cf59cc9e39a857bb54d
Meti-Adane <metiade7@gmail.com>
2021-09-07 00:36:32 +0300

    update garage incoming request on placed orders

--- road_side_API/app/controllers/order.js
+++ road_side_API/app/controllers/order.js
@@ -3,6 +3,7 @@ import Garage from "../model/Garage.js";
 import Order from "../model/Order.js";
 import Service from "../model/Service.js";
 import Location from "../model/Location.js";
+import { ObjectId } from "bson";
 
 //  place order with isPlaced = pending
 //  notify garage
@@ -13,7 +14,6 @@ import Location from "../model/Location.js";
 // order status should be -- inprogress or completed to be changed on cancel order and complete order
 
 export const placeOrder = async (req, res) => {
-  console.log("place order called");
   let order = req.body;
   if (!(Object.keys(order).length >= 3))
     return res.status(400).send("BAD REQUEST missing inputs").end();
@@ -26,11 +26,84 @@ export const placeOrder = async (req, res) => {
   if (typeof order.issued_date !== Date) {
     order.issued_date = Date.now();
   }
+  const userHasPendingOrder = await Order.findOne({
+    user_id: req.body.user_id,
+    is_pending: "pending",
+  });
+
+  if (
+    !(
+      ObjectId.isValid(req.body.user_id) &&
+      ObjectId.isValid(req.body.garage_id) &&
+      ObjectId.isValid(req.body.service_id)
+    )
+  )
+    return res.status(422).send({ message: "CONFLICT" }).end();
+  if (userHasPendingOrder)
+    return res
+      .status(409)
+      .send({ message: "CONFLICT user has pending request" })
+      .end();
 
   const newOrder = await new Order(order).save();
 
   if (!newOrder) {
     return res.status(500).send({ message: "SERVER INTERNAL ERROR" });
   }
+
+  const updateDocument = {
+    $push: { incoming_requests: order.garage_id },
+  };
+  const notifyGargae = await Garage.findByIdAndUpdate(
+    order.garage_id,
+    updateDocument
+  );
   return res.status(201).send({ message: "CREATED ", order: newOrder });
 };
+
+export const getIncomingOrder = (req, res) => {
+  const garage_id = req.params.id;
+
+  if (!garage_id) res.statu(400).send("BAD REQUEST");
+
+  Order.findOne({ garage_id: garage_id, is_pending: true }, (err, order) => {
+    if (err) res.status(500).send(err || "INTERNAL SERVER ERROR");
+    res.status(200).send({
+      message: "OK",
+      pending_order: order,
+    });
+  });
+};
+
+export const completeOrder = (req, res) => {
+  const order_id = req.body.order_id;
+  const garage_id = req.body.garage_id;
+  const updateDocument = { completed_at: req.body.completed_at || Date.now };
+
+  const query = { id: order_id, garage_id: garage_id };
+  Order.updateOne(query, updateDocument, (err, data) => {
+    if (!data)
+      res
+        .status(404)
+        .send("CONTENT NOT FOUND couldnt find order with garage id");
+    res.status(200).send({
+      message: "OK order marked as complete",
+    });
+  });
+};
+
+export const cancelOrder = (req, res) => {
+  const { user_id, order_id } = req.body;
+  const query = { user_id: user_id, id: order_id, is_pending: true };
+  Order.deleteOne(query, (err, data) => {
+    if (!data)
+      res
+        .status(404)
+        .send("CONTENT NOT FOUND order already placed / doesnt exist");
+    res.status(204).send({
+      message: "NO CONTENT order deleted",
+    });
+  });
+};
+
+export const acceptOrder = async (req, res) => {};
